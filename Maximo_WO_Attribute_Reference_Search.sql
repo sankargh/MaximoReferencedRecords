afc118bc1123d226b0a6370911585ede435e6a43
sankargh <99525703+sankargh@users.noreply.github.com>
2025-11-10 19:21:26 +0800

    Add files via upload

---
+++ Maximo_WO_Attribute_Reference_Search.sql
@@ -0,0 +1,10 @@
+--Generate select queries to find WO reference in all tables--
+
+--MS-SQL--
+select 'select '+cast((ROW_NUMBER() OVER(ORDER BY objectname ASC)) as varchar)+' , * from '+objectname+' (nolock) where '+attributename+'=''W00001'' and siteid=''SITE01'';', * from maxattributecfg (nolock) where attributename in ('WONUM','REFWO','RECORDKEY')
+and persistent=1;
+
+
+--Oracle--
+select 'SELECT * FROM '||OBJECTNAME||' WHERE '||ATTRIBUTENAME||'=''W00001'' and SITEID=''SITE01'';' as Script,
+OBJECTNAME,ATTRIBUTENAME  from MAXATTRIBUTECFG where ATTRIBUTENAME in ('WONUM','REFWO','RECORDKEY');

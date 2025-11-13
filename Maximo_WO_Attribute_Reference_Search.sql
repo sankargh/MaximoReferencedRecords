29b3f247716941fc96ae8937e17c11aacb9be9cc
sankargh <99525703+sankargh@users.noreply.github.com>
2025-11-13 16:24:53 +0800

    Update Maximo_WO_Attribute_Reference_Search.sql
    Update-1

--- Maximo_WO_Attribute_Reference_Search.sql
+++ Maximo_WO_Attribute_Reference_Search.sql
@@ -1,13 +1,33 @@
 --Generate select queries to find WO reference in all tables--
 
---MS-SQL--
-select 'select * from '+objectname+' (nolock) where '+attributename+'=''W00001'' and siteid=''SITE01'';', * 
+--#####--MS-SQL--#####----
+
+--Search by WONUM,SITEID--
+select 'select * from '+objectname+' (nolock) where '+attributename+'=''W00001'' and siteid=''SITE01'';' as Query, * 
 from maxattributecfg (nolock) 
 where attributename in ('WONUM','REFWO','RECORDKEY')
 and persistent=1;
 
---Oracle--
-select 'SELECT * FROM '||OBJECTNAME||' WHERE '||ATTRIBUTENAME||'=''W00001'' and SITEID=''SITE01'';' as Script,OBJECTNAME,ATTRIBUTENAME 
+--Search by WORKORDERID--
+declare @workorderid varchar(20); set @workorderid='100001';
+declare @ownertable varchar(20); set @ownertable='WORKORDER';
+
+select 'select * from '+objectname+' (nolock) where '+attributename+'='''+@workorderid+''' and ownertable='''+@ownertable+''';' as Query, * 
+from maxattributecfg (nolock) 
+where attributename in ('OWNERID') objectname in ('COMMLOG','ESCCOMMLOG','DOCLINKS','WFTRANSACTION','WFINSTANCE')
+and persistent=1;
+
+--#####--Oracle--#####----
+
+--Search by WONUM,SITEID--
+select 'SELECT * FROM '||OBJECTNAME||' WHERE '||ATTRIBUTENAME||'=''W00001'' and SITEID=''SITE01'';' as Query,OBJECTNAME,ATTRIBUTENAME 
 from MAXATTRIBUTECFG 
 where ATTRIBUTENAME in ('WONUM','REFWO','RECORDKEY')
 and persistent=1;
+
+--Search by WORKORDERID--
+select 'SELECT * FROM '||OBJECTNAME||' WHERE '||ATTRIBUTENAME||'=''100001'' and OWNERTABLE=''WORKORDER'';' as Query,OBJECTNAME,ATTRIBUTENAME 
+from MAXATTRIBUTECFG 
+where ATTRIBUTENAME in ('OWNERID') OBJECTNAME in ('COMMLOG','ESCCOMMLOG','DOCLINKS','WFTRANSACTION','WFINSTANCE')
+and persistent=1;
+
